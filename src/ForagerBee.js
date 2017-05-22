7a7e4c2e4a4315beee503d344169776df618ba96
cen19 <wilsoncen@gmail.com>
2017-05-22 16:39:23 -0700

    ForagerBee_halfway progress

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,10 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureCheat = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,14 @@
 var RetiredForagerBee = function() {
+  HoneyMakerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+// RetiredForagerBee.prototype.forage = function () {
+//   return 'I am too old, let me play cards instead.';
+// };
